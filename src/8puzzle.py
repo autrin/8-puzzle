41f04d19742d7ed8684d4622cb085f4823e37de7
Autrin <autrinhakimi@gmail.com>
2024-02-23 00:23:06 -0600

    working on actions

--- src/8puzzle.py
+++ src/8puzzle.py
@@ -1,26 +1,26 @@
 import argparse
-import time
-import matplotlib.pyplot as plt
-import random
 import heapq
 import math
+import queue
+import random
+import time
 import sys
+import os
 from collections import defaultdict, deque, Counter
-from itertools import combinations
-from statistics import mean 
-from functools import lru_cache
 from multiprocessing import Process, Queue
 
-class Problem(object):
-    """The abstract class for a formal problem. A new domain subclasses this,
-    overriding `actions` and `results`, and perhaps other methods.
-    The default heuristic is 0 and the default action cost is 1 for all states.
-    When yiou create an instance of a subclass, specify `initial`, and `goal` states 
-    (or give an `is_goal` method) and perhaps other keyword args for the subclass."""
+from numpy import mean
 
-    def __init__(self, initial=None, goal=None, **kwds): 
-        self.__dict__.update(initial=initial, goal=goal, **kwds) 
-        
+
+class Problem(object):
+
+    def __init__(self, initial=None, goal=None,node_counter=0, **kwds): 
+        self.initial = initial
+        self.goal = goal
+        self.node_counter = 0  # Explicitly initialize node_counter here
+        for key, value in kwds.items():
+            setattr(self, key, value)
+
     def actions(self, state):        raise NotImplementedError
     def result(self, state, action): raise NotImplementedError
     def is_goal(self, state):        return state == self.goal
@@ -32,9 +32,11 @@ class Problem(object):
             type(self).__name__, self.initial, self.goal)
     
 class Node:
-    "A Node in a search tree."
     def __init__(self, state, parent=None, action=None, path_cost=0):
-        self.__dict__.update(state=state, parent=parent, action=action, path_cost=path_cost)
+        self.state = state
+        self.parent = parent
+        self.action = action
+        self.path_cost = path_cost
 
     def __repr__(self): return '<{}>'.format(self.state)
     def __len__(self): return 0 if self.parent is None else (1 + len(self.parent))
@@ -52,29 +54,13 @@ def expand(problem, node):
         s1 = problem.result(s, action)
         cost = node.path_cost + problem.action_cost(s, action, s1)
         yield Node(s1, node, action, cost)
-        
-# def path_actions(node):
-#     """Return a string of actions to get to this node."""
-#     if node.parent is None:
-#         return ""
-#     actions = path_actions(node.parent) + [node.action]
-#     # Map the internal action representation to the project's specified format
-#     action_map = {
-#         (0, -1): 'U',
-#         (0, 1): 'D',
-#         (-1, 0): 'L',
-#         (1, 0): 'R',
-#     }
-#     return ''.join(action_map[action] for action in actions if action in action_map)
 
+# Adjust path_actions to accumulate directional actions
 def path_actions(node):
-    """Generate a sequence of number tile movements that led to this node."""
     if node.parent is None:
-        return ""
+        return []
     else:
-        # Assuming node.action now represents the tile that moved
-        return path_actions(node.parent) + str(node.action)
-
+        return path_actions(node.parent) + [node.action]
 
 def path_states(node):
     "The sequence of states to get to this node."
@@ -115,6 +101,7 @@ def best_first_search(problem, f):
     "Search nodes with minimum f(node) value first."
     node = Node(problem.initial)
     frontier = PriorityQueue([node], key=f)
+    problem.node_counter += 1
     reached = {problem.initial: node}
     while frontier:
         node = frontier.pop()
@@ -125,12 +112,14 @@ def best_first_search(problem, f):
             if s not in reached or child.path_cost < reached[s].path_cost:
                 reached[s] = child
                 frontier.add(child)
+                problem.node_counter += 1
     return failure
 
 
 def best_first_tree_search(problem, f):
     "A version of best_first_search without the `reached` table."
     frontier = PriorityQueue([Node(problem.initial)], key=f)
+    problem.node_counter += 1
     while frontier:
         node = frontier.pop()
         if problem.is_goal(node.state):
@@ -138,6 +127,7 @@ def best_first_tree_search(problem, f):
         for child in expand(problem, node):
             if not is_cycle(child):
                 frontier.add(child)
+                problem.node_counter += 1
     return failure
 
 
@@ -182,6 +172,7 @@ def breadth_first_search(problem):
     if problem.is_goal(problem.initial):
         return node
     frontier = FIFOQueue([node])
+    problem.node_counter += 1
     reached = {problem.initial}
     while frontier:
         node = frontier.pop()
@@ -192,6 +183,7 @@ def breadth_first_search(problem):
             if s not in reached:
                 reached.add(s)
                 frontier.appendleft(child)
+                problem.node_counter += 1
     return failure
 
 
@@ -206,6 +198,7 @@ def iterative_deepening_search(problem):
 def depth_limited_search(problem, limit=10):
     "Search deepest nodes in the search tree first."
     frontier = LIFOQueue([Node(problem.initial)])
+    problem.node_counter += 1
     result = failure
     while frontier:
         node = frontier.pop()
@@ -216,6 +209,7 @@ def depth_limited_search(problem, limit=10):
         elif not is_cycle(node):
             for child in expand(problem, node):
                 frontier.append(child)
+                problem.node_counter += 1
     return result
 
 
@@ -255,12 +249,11 @@ class EightPuzzle(Problem):
     """
 
     def __init__(self, initial, goal=(1, 2, 3, 4, 5, 6, 7, 8, 0)):
-        # if not self.check_solvable(initial):
-        #     print("The puzzle is unsolvable.")
-        #     sys.exit()
-        assert self.check_solvable(initial) % 2 == self.check_solvable(self.goal) % 2  # Parity check. Initial and goal states must have the same parity.
-        # self.initial, self.goal = initial, goal
-        super().__init__(initial, goal)
+        super().__init__(initial, goal)  # Call the superclass __init__ if necessary
+        self.node_counter = 0
+        assert self.check_solvable(initial) % 2 == self.check_solvable(goal) % 2, "Initial and goal states must have the same parity."
+        self.initial = initial
+        self.goal = goal
 
     def find_blank_space(self, state):
         """Return the index of the blank space (_ or 0)."""
@@ -279,12 +272,12 @@ class EightPuzzle(Problem):
         
         return [tile for tile in possible_moves if tile != 0]  # Exclude '0' from possible moves
 
-    # def result(self, state, action):
-    #     """Swap the blank with the square numbered `action`."""
-    #     s = list(state)
-    #     blank = state.index(0)
-    #     s[action], s[blank] = s[blank], s[action]
-    #     return tuple(s)
+    """
+    "U" in this action sequence means the tile below the blank space moved up.
+    "D" means the tile above the blank space moved down.
+    "L" means the tile to the right of the blank space moved left.
+    "R" means the tile to the left of the blank space moved right.
+    """
 
     def result(self, state, action):
         """Return the state that results from moving a tile into the blank space."""
@@ -318,7 +311,7 @@ class EightPuzzle(Problem):
         # Exclude the empty tile (0) before calculating inversions
         puzzle_without_empty = [num for num in puzzle if num != 0]
         _, total_inversions = count_rank_inversion(puzzle_without_empty)
-        return total_inversions % 2 == 0
+        return total_inversions
 
     def linear_conflict(self, node):
         """Calculate the total linear conflict in rows and columns."""
@@ -411,11 +404,11 @@ class Board(defaultdict):
 
 # Some specific EightPuzzle problems
 
-e1 = EightPuzzle((1, 4, 2, 0, 7, 5, 3, 6, 8))
-e2 = EightPuzzle((1, 2, 3, 4, 5, 6, 7, 8, 0))
-e3 = EightPuzzle((4, 0, 2, 5, 1, 3, 7, 8, 6))
-e4 = EightPuzzle((7, 2, 4, 5, 0, 6, 8, 3, 1))
-e5 = EightPuzzle((8, 6, 7, 2, 5, 4, 3, 0, 1))
+# e1 = EightPuzzle((1, 4, 2, 0, 7, 5, 3, 6, 8))
+# e2 = EightPuzzle((1, 2, 3, 4, 5, 6, 7, 8, 0))
+# e3 = EightPuzzle((4, 0, 2, 5, 1, 3, 7, 8, 6))
+# e4 = EightPuzzle((7, 2, 4, 5, 0, 6, 8, 3, 1))
+# e5 = EightPuzzle((8, 6, 7, 2, 5, 4, 3, 0, 1))
 
 
 # Read input
@@ -446,8 +439,8 @@ def ask_algo():
 
 # Solve an 8 puzzle problem and print out each state
 
-for s in path_states(astar_search(e1)):
-    print(board8(s))
+# for s in path_states(astar_search(e1)):
+#     print(board8(s))
 
 class CountCalls:
     """Delegate all attribute gets to the object, and count them in ._counts"""
@@ -486,88 +479,133 @@ def report_counts(counts, name):
         )
     )
 
-def astar_misplaced_tiles(problem):
-    return astar_search(problem, h=problem.h1)
+# def astar_misplaced_tiles(problem):
+#     return astar_search(problem, h=problem.h1)
 
 
-report(
-    [breadth_first_search, astar_misplaced_tiles, astar_search], [e1, e2, e3, e4, e5]
-)
+# report(
+#     [breadth_first_search, astar_misplaced_tiles, astar_search], [e1, e2, e3, e4, e5]
+# )
 
-report([astar_search, astar_tree_search], [e1, e2, e3, e4])
+# report([astar_search, astar_tree_search], [e1, e2, e3, e4])
 
-def build_table(table, depth, state, problem):
-    if depth > 0 and state not in table:
-        problem.initial = state
-        table[state] = len(astar_search(problem))
-        for a in problem.actions(state):
-            build_table(table, depth - 1, problem.result(state, a), problem)
-    return table
-
-
-def invert_table(table):
-    result = defaultdict(list)
-    for key, val in table.items():
-        result[val].append(key)
-    return result
+# def build_table(table, depth, state, problem):
+#     if depth > 0 and state not in table:
+#         problem.initial = state
+#         table[state] = len(astar_search(problem))
+#         for a in problem.actions(state):
+#             build_table(table, depth - 1, problem.result(state, a), problem)
+#     return table
 
 
-goal = (1, 2, 3, 4, 5, 6, 7, 8, 0)
-table8 = invert_table(build_table({}, 25, goal, EightPuzzle(goal)))
-
-def report8(
-    table8,
-    M,
-    Ds=range(2, 25, 2),
-    searchers=(breadth_first_search, astar_misplaced_tiles, astar_search),
-):
-    "Make a table of average nodes generated and effective branching factor"
-    for d in Ds:
-        line = [d]
-        N = min(M, len(table8[d]))
-        states = random.sample(table8[d], N)
-        for searcher in searchers:
-            nodes = 0
-            for s in states:
-                problem = CountCalls(EightPuzzle(s))
-                searcher(problem)
-                nodes += problem._counts["result"]
-            nodes = int(round(nodes / N))
-            line.append(nodes)
-        line.extend([ebf(d, n) for n in line[1:]])
-        print("{:2} & {:6} & {:5} & {:5} && {:.2f} & {:.2f} & {:.2f}".format(*line))
+# def invert_table(table):
+#     result = defaultdict(list)
+#     for key, val in table.items():
+#         result[val].append(key)
+#     return result
 
 
-def ebf(d, N, possible_bs=[b / 100 for b in range(100, 300)]):
-    "Effective Branching Factor"
-    return min(possible_bs, key=lambda b: abs(N - sum(b**i for i in range(1, d + 1))))
+# goal = (1, 2, 3, 4, 5, 6, 7, 8, 0)
+# table8 = invert_table(build_table({}, 25, goal, EightPuzzle(goal)))
+
+# def report8(
+#     table8,
+#     M,
+#     Ds=range(2, 25, 2),
+#     searchers=(breadth_first_search, astar_misplaced_tiles, astar_search),
+# ):
+#     "Make a table of average nodes generated and effective branching factor"
+#     for d in Ds:
+#         line = [d]
+#         N = min(M, len(table8[d]))
+#         states = random.sample(table8[d], N)
+#         for searcher in searchers:
+#             nodes = 0
+#             for s in states:
+#                 problem = CountCalls(EightPuzzle(s))
+#                 searcher(problem)
+#                 nodes += problem._counts["result"]
+#             nodes = int(round(nodes / N))
+#             line.append(nodes)
+#         line.extend([ebf(d, n) for n in line[1:]])
+#         print("{:2} & {:6} & {:5} & {:5} && {:.2f} & {:.2f} & {:.2f}".format(*line))
+
+
+# def ebf(d, N, possible_bs=[b / 100 for b in range(100, 300)]):
+#     "Effective Branching Factor"
+#     return min(possible_bs, key=lambda b: abs(N - sum(b**i for i in range(1, d + 1))))
 
 
 # def edepth_reduction(d, N, b=2.67):
 
 
-def random_state():
-    x = list(range(9))
-    random.shuffle(x)
-    return tuple(x)
+# def random_state():
+#     x = list(range(9))
+#     random.shuffle(x)
+#     return tuple(x)
 
 
-meanbf = mean(len(e3.actions(random_state())) for _ in range(10000))
+# meanbf = mean(len(e3.actions(random_state())) for _ in range(10000))
 # meanbf
+
+def heuristic_h1(problem):
+    return astar_search(problem, h=problem.h1)
+def heuristic_h2(problem):
+    return astar_search(problem, h=problem.h2)
+def heuristic_h3(problem):
+    return astar_search(problem, h=problem.h3)
 
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='Solve the 8-puzzle problem with various algorithms.')
-    parser.add_argument('--fPath', type=str, help='File path of the puzzle to solve.')
-    parser.add_argument('--alg', type=str, choices=['BFS', 'IDS', 'h1', 'h2', 'h3'], help='Algorithm to use for solving.')
+    parser = argparse.ArgumentParser(description='Solve the 8-puzzle problem.')
+    parser.add_argument('--fPath', type=str, help='File path of the puzzle.')
+    parser.add_argument('--alg', type=str, choices=['BFS', 'IDS', 'h1', 'h2', 'h3'], help='Algorithm.')
     return parser.parse_args()
 
+
 def run_search_algorithm(search_func, problem, result_queue):
+    start_time = time.time()
     try:
-        result = search_func(problem)
-        result_queue.put(result)  # Store the result in the queue
+        # Initialize the node counter in the problem instance
+        problem.node_counter = 0
+        solution = search_func(problem)
+
+        # Calculate the total time taken for the search
+        total_time = time.time() - start_time
+
+        if solution in [failure, cutoff]:
+            # The search failed to find a solution
+            result_queue.put({
+                "status": "failed",
+                "nodes_generated": problem.node_counter,
+                "total_time": total_time,
+                "path": [],
+            })
+        else:
+            # A solution was found
+            path = path_actions(solution)  # Convert the solution path to a sequence of actions
+            result_queue.put({
+                "status": "solved",
+                "nodes_generated": problem.node_counter,
+                "total_time": total_time,
+                "path": path,
+            })
     except Exception as e:
-        result_queue.put(e)  # Store the exception if something went wrong
+        # In case of any exception, send it back to the main process
+        result_queue.put({
+            "status": "error",
+            "message": str(e),
+        })
+
+
+algorithm_map = {
+    'BFS': breadth_first_search,
+    'IDS': iterative_deepening_search,
+    'h1': heuristic_h1,
+    'h2': heuristic_h2,
+    'h3': heuristic_h3,
+}
+
 
 def main():
     args = parse_arguments()
@@ -576,66 +614,56 @@ def main():
     puzzle = get_puzzle(args.fPath)
     if puzzle is None:
         print("Could not load the puzzle.")
-        return  # Exit if the puzzle could not be loaded
+        return
 
     puzzle_instance = EightPuzzle(puzzle)
     
     # Check if the puzzle is solvable
-    if not puzzle_instance.check_solvable(puzzle):
-        print("The puzzle is not solvable.")
+    if puzzle_instance.check_solvable(puzzle) % 2 != 0:
+        print("The inputted puzzle is not solvable:")
+        board8(puzzle)
         return
-
-    # Mapping from algorithm name to function
-    algorithm_map = {
-        'BFS': breadth_first_search,
-        'IDS': iterative_deepening_search,
-        'h1': lambda p: astar_search(p, h=p.h1),
-        'h2': lambda p: astar_search(p, h=p.h2),
-        'h3': lambda p: astar_search(p, h=p.h3),
-    }
 
     search_func = algorithm_map.get(args.alg)
     if search_func is None:
         print("Invalid algorithm specified.")
         return
 
-    # Create a queue to store the result of the search algorithm
     result_queue = Queue()
-
-    # Start measuring time
     start_time = time.time()
 
     # Create and start the search process
     search_process = Process(target=run_search_algorithm, args=(search_func, puzzle_instance, result_queue))
     search_process.start()
+    search_process.join(timeout=900)  # 15 minutes
 
-    # Wait for the process to complete or timeout
-    search_process.join(timeout=900)  # 15 minutes expressed in seconds
+    total_time = time.time() - start_time
+    seconds = int(total_time)
+    microseconds = int((total_time - seconds) * 1_000_000)
 
-    # Stop measuring time
-    end_time = time.time()
-    total_time = end_time - start_time
-
-    # Check if the process is still alive (which means it timed out)
     if search_process.is_alive():
+        # If process is still alive after the timeout
         search_process.terminate()
         search_process.join()
-        print("Algorithm timed out after 15 minutes.")
+        print(f"Algorithm timed out after 15 minutes. Total time taken: >15 min")
+        print("Total nodes generated: ", "Data unavailable — process was terminated")
+        print("Path length: Timed out.")
+        print("Path: Timed out.")
     else:
-        # Process completed within the time limit, get the result
-        result = result_queue.get()
-        if isinstance(result, Exception):
-            raise result
-        elif result == failure:
-            print("No solution found.")
-        elif result == cutoff:
-            print("Search was cut off.")
-        else:
-            # Output the results
-            print(f"Total nodes generated: {len(path_actions(result))}")
-            print(f"Total time taken: {total_time:.2f} seconds")
-            path = path_actions(result)  # Get the actions that led to the solution
-            print(f"Path: {''.join(path)}")
+        # Process completed within the time limit
+        try:
+            result = result_queue.get_nowait()
+            if isinstance(result, Exception):
+                raise result
+            elif result == failure or result == cutoff:
+                print("No solution found or search was cut off.")
+            else:
+                print(f"Total nodes generated: {result['nodes_generated']}")
+                print(f"Total time taken: {seconds} sec {microseconds} microSec.")
+                print(f"Path length: {len(result['path'])}")
+                print(f"Path: {''.join(result['path'])}")
+        except queue.Empty:
+            print("No result was returned by the search algorithm.")
 
 if __name__ == '__main__':
     main()
